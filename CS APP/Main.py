377122e1ab42d9a49b8d7585842465676a4e1496
Earl Batenga <61070797+Elchupappi@users.noreply.github.com>
2020-11-18 23:24:29 +0800

    employeeadminupdate

--- CS APP/Main.py
+++ CS APP/Main.py
@@ -201,6 +201,11 @@ def save_button():
     with open("CS APP/sickness.txt", "w") as outf:
         outf.write(text)
         ctypes.windll.user32.MessageBoxW(0, "Done, Changes Saved!", "ADD", 0)
+def save_button_emp():
+    textemp = textfieldsyemployadddel.get("1.0",'end-1c')
+    with open("CS APP/sickness.txt", "w") as outf:
+        outf.write(textemp)
+        ctypes.windll.user32.MessageBoxW(0, "Done, Changes Saved!", "ADD", 0)
 
 
 def add_delete_sick():
@@ -256,42 +261,10 @@ def show_sick():
     textfieldsyadmin.insert(tk.END, showread)
     textfieldsyadmin.config(state=DISABLED)
 
-def admin_main():
-    global clockkk
-    global canvappadmin
-    root.title("M.H.M.S --ADMIN--")
-    Menus()
-    #---------------btnCanvas------------------------------------------
-    canvappadmin = tk.Canvas(root, width="1220",height="1220",relief = tk.FLAT, bg='#82a8C4')
-    canvas.create_window(520,370,window=canvappadmin)
-
-    clockkk = tk.Label(root,font=('times', 10, 'bold'), bg='#82a8C4')
-    canvas.create_window(890,-3,window=clockkk)
-    adminticks()
-
-    nameappadmin = tk.Label(root,font=('times', 10, 'bold'), bg='#82a8C4', text="Medical Health Monitoring System")
-    canvas.create_window(120,-3,window=nameappadmin)
-
-    nameappadministrator = tk.Label(root,font=('times', 15, 'bold'), bg='#82a8C4', text="Administrator")
-    canvas.create_window(520,-3,window=nameappadministrator)
-
-    searchadmin = tk.Button(root, text =" Sickness ", command = show_sick, width=57,height=2, font=('times', 20, 'bold'))
-    canvas.create_window(70,58,window=searchadmin)
-
-    libraradmin = tk.Button(root, text =" Add/Delete", command = add_delete_sick, width=57,height=2, font=('times', 20, 'bold'))
-    canvas.create_window(80,150,window=libraradmin)
-
-    logoutadmin = tk.Button(root, text =" Log-out ", command = Exit, width=57,height=2, font=('times', 20, 'bold'))
-    canvas.create_window(64,242,window=logoutadmin)
-
-    #---------------btnCanvas------------------------------------------
-    canvappsearchadmin = tk.Canvas(root, width="750",height="570",relief = tk.FLAT, background="#C8EBE9")
-    canvappadmin.create_window(710,545,window=canvappsearchadmin)
-
 
 #---------------btnFunctionsStart------------------------------------------
-def search_command2(event=None):
-    x = searchsick.get()
+def search_command2():
+    x = searchsick.get().lower()
     textfield.delete(0, 'end')
 
     if x:
@@ -374,6 +347,173 @@ def Sickness():
     ctypes.windll.user32.MessageBoxW(0, "Insert Sickness here", "Sickness", 0)
 def About():
     ctypes.windll.user32.MessageBoxW(0, "This app is for finding sickness by searching symptoms.", "About", 0)
+def employee_register():
+    employz = usernameRegisteremp.get()
+    employx = passwordRegisteremp.get()
+    logusnemploy = open('CS APP/employee_usn.txt','r')
+    logpasemploy = open('CS APP/employee_pass.txt','r')
+    logusnlineemploy= logusnemploy.readline() 
+    logpaslineemploy = logpasemploy.readline() 
+    stripusnemploy = logusnlineemploy.split(" ")
+    strippasemploy = logpaslineemploy.split(" ")
+
+    if employz in stripusnemploy and employx in strippasemploy:
+        if  employz == '' and employx == '':
+            ctypes.windll.user32.MessageBoxW(0, "No input", "Login Error", 0)
+            return
+        elif employz == '':
+            ctypes.windll.user32.MessageBoxW(0, "No Username", "Login Error", 0)
+        elif employx == '':
+            ctypes.windll.user32.MessageBoxW(0, "No Password", "Login Error", 0)
+        else:
+            employee_main()
+            return
+    elif employz not in stripusnemploy and employx not in strippasemploy:
+        ctypes.windll.user32.MessageBoxW(0, "No Account like that has been registered.", "Login",0)
+    else:
+        ctypes.windll.user32.MessageBoxW(0, "Incorrect Username or Password.", "Login",0)
+        return
+def employee_login():
+    global registerbtn
+    global canvasRegisteremp
+    global usernameRegisteremp
+    global passwordRegisteremp
+    canvasRegisteremp = tk.Canvas(root, width="220",height="220",relief = tk.FLAT)
+    canvas.create_window(520,370,window=canvasRegisteremp)
+    #--------------------------Register/Start----------------------------
+    usnRegisteremp = tk.Label(root, text="Username")
+    canvasRegisteremp.create_window(110,80,window=usnRegisteremp) 
+
+    usernameRegisteremp = tk.Entry(root)
+    canvasRegisteremp.create_window(110,100,window=usernameRegisteremp)
+
+    pswRegisteremp = tk.Label(root, text="Password")
+    canvasRegisteremp.create_window(110,130,window=pswRegisteremp)
+    passwordRegisteremp = tk.Entry(root)
+    canvasRegisteremp.create_window(110,170,window=passwordRegisteremp)
+
+    registertxtemp = tk.Label(root, text="EMPLOYEE", font=("Arial", 26))
+    canvasRegisteremp.create_window(110,35,window=registertxtemp)
+
+    loginRegemp = tk.Button(root, text ="Log-in", command = employee_register)
+    canvasRegisteremp.create_window(60,200,window=loginRegemp)  
+    registerRegemp= tk.Button(root, text =" Cancel ", command =  cancelregister)
+    canvasRegisteremp.create_window(160,200,window=registerRegemp)
+def employee_main():
+    global clocke
+    global canvappemployee
+    root.title("M.H.M.S --EMPLOYEE--")
+    Menus()
+    #---------------btnCanvas------------------------------------------
+    canvappemployee = tk.Canvas(root, width="1220",height="1220",relief = tk.FLAT, bg='#82a8C4')
+    canvas.create_window(520,370,window=canvappemployee)
+
+    clocke = tk.Label(root,font=('times', 10, 'bold'), bg='#82a8C4')
+    canvas.create_window(890,-3,window=clocke)
+    employticks()
+
+    nameappemp = tk.Label(root,font=('times', 10, 'bold'), bg='#82a8C4', text="Medical Health Monitoring System")
+    canvas.create_window(120,-3,window=nameappemp)
+
+    nameappemployee = tk.Label(root,font=('times', 15, 'bold'), bg='#82a8C4', text="Employee")
+    canvas.create_window(520,-3,window=nameappemployee)
+
+    searchemp = tk.Button(root, text =" Sickness ", command = show_sick_employee, width=57,height=2, font=('times', 20, 'bold'))
+    canvas.create_window(70,58,window=searchemp)
+
+    libraremp= tk.Button(root, text =" Add/Delete", command = add_delete_sick_employee, width=57,height=2, font=('times', 20, 'bold'))
+    canvas.create_window(80,150,window=libraremp)
+
+    logoutemp= tk.Button(root, text =" Log-out ", command = Exit, width=57,height=2, font=('times', 20, 'bold'))
+    canvas.create_window(64,243,window=logoutemp)
+
+    #---------------btnCanvas------------------------------------------
+    canvappsearchemp = tk.Canvas(root, width="750",height="570",relief = tk.FLAT, background="#C8EBE9")
+    canvappemployee.create_window(710,545,window=canvappsearchemp)
+def show_sick_employee():
+    canvasempsymp = tk.Canvas(root, width="750",height="570",relief = tk.FLAT, background="#C8EBE9")
+    canvappemployee.create_window(710,545,window=canvasempsymp)
+
+    labelemplsym = tk.Label(root,font=('times', 10, 'bold'), bg='#82a8C4', text="Sickness Library", width="105",height="2")
+    canvasempsymp.create_window(377,30,window=labelemplsym)
+    #----------------this part read and show output--------------------
+    scrollemplsymp = tk.Scrollbar(root)
+    textfieldsyempl= tk.Text(root, height=32, width=92)
+    canvasempsymp.create_window(377,310,window=scrollemplsymp)
+    canvasempsymp.create_window(377,310,window=textfieldsyempl)
+    scrollemplsymp.config(command=textfieldsyempl.yview)
+    textfieldsyempl.config(yscrollcommand=scrollemplsymp.set)
+
+    readsickempl = open('CS APP/sickness.txt','r')
+    readsicknesempl = readsickempl.read() 
+    quote = """M.H.M.S Sickness Library."""
+    showread = quote + "\n" + readsicknesempl
+    textfieldsyempl.insert(tk.END, showread)
+    textfieldsyempl.config(state=DISABLED)
+def add_delete_sick_employee():
+    global textfieldsyemployadddel
+    canvasemployeeui = tk.Canvas(root, width="750",height="570",relief = tk.FLAT, background="#C8EBE9")
+    canvappemployee.create_window(710,545,window=canvasemployeeui)
+
+    labelemployadddel = tk.Label(root,font=('times', 10, 'bold'), bg='#82a8C4', text="Sickness Add/Delete", width="105",height="2")
+    canvasemployeeui.create_window(377,30,window=labelemployadddel)
+    #----------------this part read and show output--------------------
+
+
+    scrollemployadddel = tk.Scrollbar(root)
+    readsickshowemploy = open('CS APP/sickness.txt','r')
+    readsicknesshowemploy = readsickshowemploy.read() 
+    textfieldsyemployadddel = tk.Text(root, height=23, width=92)
+    canvasemployeeui.create_window(377,315,window=scrollemployadddel)
+    canvasemployeeui.create_window(377,315,window=textfieldsyemployadddel)
+    scrollemployadddel.config(command=textfieldsyemployadddel.yview)
+    textfieldsyemployadddel.insert(tk.END, readsicknesshowemploy)
+    textfieldsyemployadddel.config(yscrollcommand=scrollemployadddel.set)
+
+    textfieldsyemployadddell = tk.Text(root, height=5, width=92)
+    canvasemployeeui.create_window(377,80,window=textfieldsyemployadddell)
+    quotee = """M.H.M.S Add/Delete Sickness (Follow the Format)."""
+    showreadd = quotee + "\n" + "Sickness_Name - (1stSymptom, 2nd, 3rd, 4th) 'Definition'"
+    textfieldsyemployadddell.insert(tk.END, showreadd)
+    textfieldsyemployadddell.config(state=DISABLED)
+
+    add = tk.Button(root, text ="  Save  ", command = save_button_emp, width=10,height=1, font=('times', 15, 'bold'))
+    canvasemployeeui.create_window(80,540,window=add)
+def admin_main():
+    global clockkk
+    global canvappadmin
+    root.title("M.H.M.S --ADMIN--")
+    Menus()
+    #---------------btnCanvas------------------------------------------
+    canvappadmin = tk.Canvas(root, width="1220",height="1220",relief = tk.FLAT, bg='#82a8C4')
+    canvas.create_window(520,370,window=canvappadmin)
+
+    clockkk = tk.Label(root,font=('times', 10, 'bold'), bg='#82a8C4')
+    canvas.create_window(890,-3,window=clockkk)
+    adminticks()
+
+    nameappadmin = tk.Label(root,font=('times', 10, 'bold'), bg='#82a8C4', text="Medical Health Monitoring System")
+    canvas.create_window(120,-3,window=nameappadmin)
+
+    nameappadministrator = tk.Label(root,font=('times', 15, 'bold'), bg='#82a8C4', text="Administrator")
+    canvas.create_window(520,-3,window=nameappadministrator)
+
+    searchadmin = tk.Button(root, text =" Sickness ", command = show_sick, width=57,height=2, font=('times', 20, 'bold'))
+    canvas.create_window(70,58,window=searchadmin)
+
+    libraradmin = tk.Button(root, text =" Add/Delete", command = add_delete_sick, width=57,height=2, font=('times', 20, 'bold'))
+    canvas.create_window(80,150,window=libraradmin)
+
+    employee = tk.Button(root, text =" Register Account ", command = admin_employee_register, width=57,height=2, font=('times', 20, 'bold'))
+    canvas.create_window(117,242,window=employee)
+
+    logoutadmin = tk.Button(root, text =" Log-out ", command = Exit, width=57,height=2, font=('times', 20, 'bold'))
+    canvas.create_window(64,332,window=logoutadmin)
+
+    #---------------btnCanvas------------------------------------------
+    canvappsearchadmin = tk.Canvas(root, width="750",height="570",relief = tk.FLAT, background="#C8EBE9")
+    canvappadmin.create_window(710,545,window=canvappsearchadmin)
+
 def AdminLogin():
     adminz = usernameadmin.get()
     adminx = passwordadmin.get()
@@ -400,7 +540,83 @@ def AdminLogin():
     else:
         ctypes.windll.user32.MessageBoxW(0, "Incorrect Username or Password.", "Login",0)
         return
+def admin_employee_register():
+    global canvasEmployee
+    global usernameemployee
+    global passwordemployee
 
+    canvasEmployee = tk.Canvas(root, width="739",height="557",relief = tk.FLAT)
+    canvas.create_window(617,305,window=canvasEmployee)
+    #--------------------------Register/Start----------------------------
+    usnemployee = tk.Label(root, text="Username")
+    canvasEmployee.create_window(380,180,window=usnemployee) 
+
+    usernameemployee = tk.Entry(root)
+    canvasEmployee.create_window(380,200,window=usernameemployee)
+
+    passemployee = tk.Label(root, text="Password")
+    canvasEmployee.create_window(380,230,window=passemployee)
+    passwordemployee = tk.Entry(root)
+    canvasEmployee.create_window(380,270,window=passwordemployee)
+
+    employeetxt = tk.Label(root, text="Add Account", font=("Arial", 26))
+    canvasEmployee.create_window(380,135,window=employeetxt)
+
+    employeelogin = tk.Button(root, text ="Register", command = admin_create_acc_employee)
+    canvasEmployee.create_window(380,300,window=employeelogin)  
+
+def admin_create_acc_employee():
+    q = usernameemployee.get()
+    e = passwordemployee.get()
+    #----------ERROR ACCEPTING MULTIPLE SPACES------------
+    if q == '    ' and e == '    ':
+        usernameemployee.delete(0, 'end')
+        passwordemployee.delete(0, 'end')
+        ctypes.windll.user32.MessageBoxW(0, "No input", "Register Error", 0)
+        return
+    if q == '   ' and e == '   ':
+        usernameemployee.delete(0, 'end')
+        passwordemployee.delete(0, 'end')
+        ctypes.windll.user32.MessageBoxW(0, "No input", "Register Error", 0)
+        return
+    if q == '  ' and e == '  ':
+        usernameemployee.delete(0, 'end')
+        passwordemployee.delete(0, 'end')
+        ctypes.windll.user32.MessageBoxW(0, "No input", "Register Error", 0)
+        return
+    if q == ' ' and e == ' ':
+        usernameemployee.delete(0, 'end')
+        passwordemployee.delete(0, 'end')
+        ctypes.windll.user32.MessageBoxW(0, "No input", "Register Error", 0)
+        return
+    elif q == '' and e == '':
+        ctypes.windll.user32.MessageBoxW(0, "No input", "Register Error", 0)
+        return
+    else:
+        if q == q and e == e:
+            readusnemp = open('CS APP/employee_usn.txt','r')
+            readpasemp = open('CS APP/employee_pass.txt','r')
+            readusnlineemp = readusnemp.readline() 
+            readpaslineemp = readpasemp.readline() 
+            stripusnreademp = readusnlineemp.split(" ")
+            strippasreademp = readpaslineemp.split(" ")
+            if len(e) != 4:
+                ctypes.windll.user32.MessageBoxW(0, "Password must be 4 digits", "Register", 0)
+                return
+
+            elif q in stripusnreademp and e in strippasreademp:
+                ctypes.windll.user32.MessageBoxW(0, "Account Already Registered", "Register",0)
+                return
+            else:
+                if len(e) == 4:
+                    regusnemp = open('CS APP/employee_usn.txt','a+')
+                    regpasemp = open('CS APP/employee_pass.txt','a+')
+                    regusnemp.write(q + " ")
+                    regpasemp.write(e + " ")
+                    regusnemp.close()
+                    regpasemp.close()
+                    ctypes.windll.user32.MessageBoxW(0, "Done, Employee Registered!", "Register", 0)
+                    return
 def Admin():
     global canvasAdmin
     global usernameadmin
@@ -438,6 +654,10 @@ def adminticks():
     nowww = datetime.now().strftime(' Date: %Y-%m-%d \n Time: %H:%M:%S')
     clockkk.config(text=nowww)
     clockkk.after(200, adminticks)
+def employticks():
+    nowwww = datetime.now().strftime(' Date: %Y-%m-%d \n Time: %H:%M:%S')
+    clocke.config(text=nowwww)
+    clocke.after(200, employticks)
 #--------------------------login/begin----------------------------
 def start():
     global clock
@@ -461,6 +681,9 @@ def start():
 
     register = tk.Button(root, text ="Register", command =  registerbtn)
     canvas.create_window(570,460,window=register)
+
+    employ = tk.Button(root, text ="Employee", command =  employee_login)
+    canvas.create_window(960,590,window=employ)
 
     admin = tk.Button(root, text ="Admin", command =  Admin)
     canvas.create_window(1018,590,window=admin)
